9cc878ac59696f57400112d23d6083f12efc898c
shimies <sshimizu.am@gmail.com>
2021-08-10 17:14:08 +0900

    Initial solution for 4.8

--- epi_judge_cpp/reverse_digits.cc
+++ epi_judge_cpp/reverse_digits.cc
@@ -1,12 +1,34 @@
 #include "test_framework/generic_test.h"
+
+static long long impl_naive(int x) {
+    auto str = std::to_string(x);
+    auto s = str.begin();
+    auto e = str.end() - 1;
+    if (*s == '-')
+        ++s;
+    while (s < e) {
+        std::swap(*s, *e);
+        ++s, --e;
+    }
+    return std::stoll(str);
+}
+
+static long long impl_improved(int x) {
+    long long y = 0;
+    while (x) {
+        y = y * 10 + x % 10;
+        x /= 10;
+    }
+    return y;
+}
+
 long long Reverse(int x) {
-  // TODO - you fill in here.
-  return 0;
+    // return impl_naive(x);
+    return impl_improved(x);
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x"};
-  return GenericTestMain(args, "reverse_digits.cc", "reverse_digits.tsv",
-                         &Reverse, DefaultComparator{}, param_names);
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"x"};
+    return GenericTestMain(args, "reverse_digits.cc", "reverse_digits.tsv", &Reverse, DefaultComparator{}, param_names);
 }
